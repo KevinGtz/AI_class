9e141a126f617887eeb48857c2ad8c49b88ddb8e
Kevin Gutierrez <kevin@novelistik.com>
2017-10-31 11:40:31 -0600

    solved the bootstrap library problems

--- production/app/forms.py
+++ production/app/forms.py
@@ -7,5 +7,5 @@ class MainForm(FlaskForm):
     number_of_cities = IntegerField("Numero de ciudades", validators=[Required()])
     number_of_colors = IntegerField("Numero de colores", validators=[Required()])
     number_of_relations = IntegerField('Numero de relaciones', validators=[Required()])
-    relations = TextAreaField('relaciones')
+    relations = TextAreaField("Relaciones; Coloque las relaciones con el siguiente formato: '1-2, 3-4, 4-5'")
     submit = SubmitField('enviar')
