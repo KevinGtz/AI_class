49a93acd3df843879a886addb75c95a21742c038
Kevin Gutierrez <kevin@novelistik.com>
2017-11-26 19:23:14 -0600

    This was the second change to the for loop

--- production/app/project.py
+++ production/app/project.py
@@ -103,11 +103,11 @@ def process_the_info(c_i, c_o, r_n, t_r):
         # We nee to put a Try and a except
         try:
             mn = int(most_common(nodes))
-            minor_n = int(less_common(nodes_4))
+            # minor_n = int(less_common(nodes_4))
         except:
             break
         print (mn, 'nodo principal')
-        print (minor_n, 'nodo menor')
+        # print (minor_n, 'nodo menor')
 
         if colors_t_ch_2 != [] and colors_t_ch_3 != []:
             if len(colors_t_ch_2) > 1:
@@ -115,7 +115,7 @@ def process_the_info(c_i, c_o, r_n, t_r):
             elif len(colors_t_ch_2) == 1:
                 mc = colors_t_ch_2[0]
 
-            minor_c = colors_t_ch_3[0]
+            # minor_c = colors_t_ch_3[0]
 
         # First section of adding the back colors
         elif colors_t_ch_2 == [] and colors_t_ch_4 != []:
@@ -124,7 +124,7 @@ def process_the_info(c_i, c_o, r_n, t_r):
             elif len(colors_t_ch_4) == 1:
                 mc = colors_t_ch_4[0]
 
-            minor_c = colors_t_ch_5[0]
+            # minor_c = colors_t_ch_5[0]
 
         # Second section of adding the back colors
         elif colors_t_ch_4 == [] and colors_t_ch_6 != []:
@@ -133,49 +133,53 @@ def process_the_info(c_i, c_o, r_n, t_r):
             elif len(colors_t_ch_6) == 1:
                 mc = colors_t_ch_6[0]
 
-            minor_c = colors_t_ch_7[0]
+            # minor_c = colors_t_ch_7[0]
 
 
         print (mc, 'color principal')
-        print (minor_c, 'color menor')
+        # print (minor_c, 'color menor')
 
         # Here we assing the main color to the main node
         for dic in relations_complete:
             for node, colors in dic.items():
                 if node == mn and colors == 'default':
-                    dic[mn] = mc
-                elif node == minor_n and colors == 'default':
-                    dic[minor_n] = minor_c
+                    while dic[node] == mc:
+                        print 'in'
+                        mc = random.choice(colors_t_ch_7)
+                        dic[node] = mc
+                    print 'out'
+                    dic[node] = mc
+                # elif node == minor_n and colors == 'default':
+                #     dic[minor_n] = minor_c
 
         for i in range(len(nodes)):
             if mn in nodes:
                 nodes.remove(mn)
-        for i in range(len(nodes_4)):
-            if minor_n in nodes_4:
-                nodes_4.remove(minor_n)
+        # for i in range(len(nodes_4)):
+        #     if minor_n in nodes_4:
+        #         nodes_4.remove(minor_n)
 
         if len(colors_t_ch_2) >= 1:
             colors_t_ch_2.remove(mc)
-        if len(colors_t_ch_3) >= 1:
-            colors_t_ch_3.remove(minor_c)
+        # if len(colors_t_ch_3) >= 1:
+        #     colors_t_ch_3.remove(minor_c)
 
         # First remove of the back colors
         if len(colors_t_ch_4) >= 1:
             colors_t_ch_4.remove(mc)
-        if len(colors_t_ch_5) >= 1:
-            colors_t_ch_5.remove(minor_c)
+        # if len(colors_t_ch_5) >= 1:
+        #     colors_t_ch_5.remove(minor_c)
 
         # Second remove of the back colors
         if len(colors_t_ch_6) >= 1:
             colors_t_ch_6.remove(mc)
-        if len(colors_t_ch_7) >= 1:
-            colors_t_ch_7.remove(minor_c)
+        # if len(colors_t_ch_7) >= 1:
+        #     colors_t_ch_7.remove(minor_c)
         print 'hello'
 
         # print relations_complete
     print 'hello2'
-
-    # print relations_complete
+    print relations_complete
 
     nodes_2 = []
     nodes_3 = []
